f7fb570940218074e3d48c2ecc5ac5f774cfc7e3
Antraxmin <Antraxmin@naver.com>
2023-06-24 17:43:36 +0900

    feat: 폰트 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230624.172836';
+const cacheName = 'chirpy-20230624.174131';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
