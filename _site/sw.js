d668acb38b890bb5255c973e6fcf6c91e5c60fa6
Antraxmin <Antraxmin@naver.com>
2023-06-24 17:29:04 +0900

    modify first post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230624.172052';
+const cacheName = 'chirpy-20230624.172836';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
