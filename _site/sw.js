260ac61aff01b329e4c3a4a46e422082b37c160d
Antraxmin <antraxmin@naver.com>
2023-06-26 14:32:52 +0900

    fix: gitignore

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230626.142204';
+const cacheName = 'chirpy-20230626.143046';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
